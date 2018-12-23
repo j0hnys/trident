69c35abe067c05a83293b91741c744918e6243c7
j0hnys <contact_me@johnstamkos.com>
2018-12-23 23:17:09 +0200

    updated export model schema

--- src/Builders/Export/Model.php
+++ src/Builders/Export/Model.php
@@ -26,11 +26,7 @@ class Model
 
         $tmp = [];
         foreach ($db_table_fillables as $key => $db_table_fillable) {
-            $tmp []= [
-                'column_name' => $db_table_fillable,
-                'column_type' => \Schema::getColumnType($db_table_name, $db_table_fillable),
-                'type' => 'fillable',
-            ];    
+            $tmp []= $this->schemaItem($db_table_fillable, \Schema::getColumnType($db_table_name, $db_table_fillable));
         }
 
         //
@@ -49,21 +45,46 @@ class Model
      * @param  string $path
      * @return string
      */
-    protected function makeDirectory($path)
+    protected function makeDirectory($path): void
     {
         if (!is_dir(dirname($path))) {
             mkdir(dirname($path), 0777, true);
         }
     }
     
-    /**
-     * Get code and save to disk
-     * @return mixed
-     * @throws \Exception
-     */
-    public function save()
+    protected function schemaItem(string $column_name = '', string $column_type = ''): array
     {
-        //
+        $item = [
+            'column_name' => $column_name,
+            'column_type' => $column_type,
+            'type' => 'fillable',
+        ];
+
+        if ($column_type == 'string') {
+            $item['validation_rules'] = [
+                "required" => true,
+                "type" => "string",
+                "trigger" => "blur",
+            ];
+            $item['attributes'] = [
+                "type" => ["string" => true],
+                "default_value" => '\'\'',
+                "element_type" => false,
+            ];
+        } else if ($column_type == 'integer') {
+            $item['validation_rules'] = [
+                "required" => true,
+                "type" => "number",
+                "trigger" => "blur",
+            ];
+            $item['attributes'] = [
+                "type" => ["number" => true],
+                "default_value" => '0',
+                "element_type" => false,
+            ];
+        }
+
+        return $item;
     }
 
 }
