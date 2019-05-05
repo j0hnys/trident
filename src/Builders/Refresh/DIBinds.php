25737ab9d334939f67a0cfaa375f2f207670fb59
unknown <johnyss@example.com>
2019-05-05 12:42:22 +0300

    workflow logic code analysis demo

--- src/Builders/Refresh/DIBinds.php
+++ src/Builders/Refresh/DIBinds.php
@@ -2,6 +2,11 @@
 
 namespace j0hnys\Trident\Builders\Refresh;
 
+use PhpParser\Error;
+use PhpParser\NodeDumper;
+use PhpParser\ParserFactory;
+use PhpParser\{Node, NodeFinder};
+
 class DIBinds
 {
     
@@ -16,6 +21,117 @@ class DIBinds
         $mustache = new \Mustache_Engine;
 
         dump('faseis');
+
+        $name = 'DemoProcess';
+
+        $code = file_get_contents( base_path().'/app/Trident/Workflows/Logic/'.ucfirst($name).'.php' );
+
+        $parser = (new ParserFactory)->create(ParserFactory::PREFER_PHP7);
+        try {
+            $ast = $parser->parse($code);
+        } catch (Error $error) {
+            echo "Parse error: {$error->getMessage()}\n";
+            return;
+        }
+
+        // $dumper = new NodeDumper;
+        // dump($ast[0]->exprs);
+        // echo $dumper->dump($ast) . "\n";
+
+
+        $analysis_result = (object)[
+            'used_namespaces' => [],
+            'constructor_params' => [],
+        ];
+
+        $nodeFinder = new NodeFinder;
+        $nodeFinder->find($ast, function(Node $node) use (&$analysis_result){
+            if ($node instanceof Node\Stmt\Use_) {
+                // dump([
+                //     // '$node->getAttributes()' => $node->getAttributes(),
+                //     // '$node->getSubNodeNames()' => $node->getSubNodeNames(),
+                //     // '$node->getLine()' => $node->getLine(),
+                //     // '$node->getType()' => $node->getType(),
+                //     // '$node->uses[0]->name->parts' => $node->uses[0]->name->parts,
+                //     '$node' => $node,
+                //     // '$node->uses[0]->name->parts' => $node->uses[0]->name->parts,
+                //     // '$node->uses[0]->alias' => $node->uses[0]->alias,
+                // ]);
+
+                $analysis_result->used_namespaces []= $node->uses[0];
+
+            }
+
+            if ($node instanceof Node\Stmt\ClassMethod) {
+                if ($node->name == '__construct') {
+                    if (!empty($node->params)) {
+                        $tmp = [];
+                        foreach ($node->params as $parameter) {
+                            $tmp []= (object)[
+                                'type' => $parameter->type,
+                                'var' => $parameter->var,
+                            ];
+                        }
+                        $analysis_result->constructor_params = $tmp;
+                    }
+                }
+            }
+
+        });
+
+        dump([
+            '$analysis_result' => $analysis_result,
+        ]);
+
+        $di_interfaces = [];
+
+        foreach ($analysis_result->constructor_params as $constructor_param) {
+            foreach ($analysis_result->used_namespaces as $used_namespace) {
+                if (count($constructor_param->type->parts) == 1) {  //dld exw alias
+                    if (!empty($used_namespace->alias)) {
+                        if ($used_namespace->alias == $constructor_param->type->parts[0]) {
+                            // dump('USED INTERFACE FOUND!! '.$used_namespace->alias.' | '.implode('\\',$used_namespace->name->parts));
+                            $di_interfaces []= [
+                                'name' => $used_namespace->name
+                            ];
+                        }
+                    }
+                } else {
+                    // TO DO
+                }
+            }
+
+        }
+
+        dump([
+            '$di_interfaces' => $di_interfaces,
+        ]);
+
+
+
+        // dump($code);
+
+        // $traverser = new NodeTraverser;
+        // $traverser->addVisitor(new class extends NodeVisitorAbstract {
+        //     public function leaveNode(Node $node) {
+
+        //         dump([
+        //             '$node' => $node,
+        //         ]);
+
+        //         // if ($node instanceof Node\Scalar\LNumber) {
+        //         //     return new Node\Scalar\String_((string) $node->value);
+        //         // }
+        //     }
+        // });
+        // $modifiedStmts = $traverser->traverse($ast);
+
+
+
+
+
+
+
 
         // //
         // //workflow logic test generation
