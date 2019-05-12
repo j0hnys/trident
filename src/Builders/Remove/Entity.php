5c0cf96b80df99727cbcda0432240ebac930cb02
unknown <johnyss@example.com>
2019-05-12 17:02:03 +0300

    remove entity start

---
+++ src/Builders/Remove/Entity.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace j0hnys\Trident\Builders\Remove;
+
+use PhpParser\Error;
+use PhpParser\NodeDumper;
+use PhpParser\ParserFactory;
+use PhpParser\{Node, NodeFinder};
+
+class Entity
+{
+    
+    /**
+     * Crud constructor.
+     * @param string $name
+     * @throws \Exception
+     */
+    public function __construct($name = 'TEST')
+    {
+        
+        dump('in here');
+
+
+    }
+
+
+
+}
