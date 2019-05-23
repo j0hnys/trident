ad9869f85eaf3d5918ad27fa926ec5d253a17fc6
unknown <contact_me@johnstamkos.com>
2019-05-23 20:24:08 +0300

    remove entity function start

---
+++ src/Console/Commands/RemoveEntityFunction.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace j0hnys\Trident\Console\Commands;
+
+use Illuminate\Console\Command;
+use j0hnys\Trident\Builders\Remove;
+
+class RemoveEntityFunction extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = "trident:remove:entity_function {name}";
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Removes trident entity\'s function with the structures connected to it.';
+    
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        try {
+            $name = $this->argument('name');
+            
+
+            $crud = new Remove\Function($name);
+            
+
+            $this->info($name.' removed successfully');
+            
+        } catch (\Exception $ex) {
+            $this->error($ex->getMessage() . ' on line ' . $ex->getLine() . ' in ' . $ex->getFile());
+        }
+    }
+
+}
