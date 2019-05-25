6b1e727526eba1bcaa1bd2e7e5ac5742c4b02c63
unknown <contact_me@johnstamkos.com>
2019-05-25 22:03:02 +0300

    resource and strict type auto completion by file

--- src/Builders/StrictType.php
+++ src/Builders/StrictType.php
@@ -26,12 +26,6 @@ class StrictType
 
             $stub = file_get_contents(__DIR__.'/../../src/Stubs/Trident/'.$domain.'/Typed/LogicStruct.stub');
 
-            $stub = str_replace('{{td_entity}}', lcfirst($td_entity_name), $stub);
-            $stub = str_replace('{{Td_entity}}', ucfirst($td_entity_name), $stub);
-            $stub = str_replace('{{function_name}}', ucfirst($function_name), $stub);
-            
-            file_put_contents($struct_path, $stub);
-
         } else if (strtolower($strict_type_name == 'collection_struct')) {
             //
             //struct logic generation
@@ -44,12 +38,6 @@ class StrictType
             $this->makeDirectory($struct_path);
 
             $stub = file_get_contents(__DIR__.'/../../src/Stubs/Trident/'.$domain.'/Typed/LogicCollectionStruct.stub');
-
-            $stub = str_replace('{{td_entity}}', lcfirst($td_entity_name), $stub);
-            $stub = str_replace('{{Td_entity}}', ucfirst($td_entity_name), $stub);
-            $stub = str_replace('{{function_name}}', ucfirst($function_name), $stub);
-            
-            file_put_contents($struct_path, $stub);
 
         } else if (strtolower($strict_type_name == 'map_struct')) {
             //
@@ -64,12 +52,6 @@ class StrictType
 
             $stub = file_get_contents(__DIR__.'/../../src/Stubs/Trident/'.$domain.'/Typed/LogicMapStruct.stub');
 
-            $stub = str_replace('{{td_entity}}', lcfirst($td_entity_name), $stub);
-            $stub = str_replace('{{Td_entity}}', ucfirst($td_entity_name), $stub);
-            $stub = str_replace('{{function_name}}', ucfirst($function_name), $stub);
-            
-            file_put_contents($struct_path, $stub);
-
         } if (strtolower($strict_type_name == 'struct_optional')) {
             //
             //struct logic generation
@@ -83,16 +65,51 @@ class StrictType
 
             $stub = file_get_contents(__DIR__.'/../../src/Stubs/Trident/'.$domain.'/Typed/LogicStructOptional.stub');
 
-            $stub = str_replace('{{td_entity}}', lcfirst($td_entity_name), $stub);
-            $stub = str_replace('{{Td_entity}}', ucfirst($td_entity_name), $stub);
-            $stub = str_replace('{{function_name}}', ucfirst($function_name), $stub);
-            
-            file_put_contents($struct_path, $stub);
-
         } else {
             throw new \Exception("unknown strict type", 1);
             
         }
+
+
+
+        $mustache = new \Mustache_Engine;
+
+
+        $schema = [];
+        $configuration = config('trident');
+        if (!empty($configuration)) {
+            if (isset($configuration['solution']['schemas']['folder'])) {
+                $tmp_schemas = $this->getFolderFiles($configuration['solution']['schemas']['folder']);
+
+                foreach ($tmp_schemas as $tmp_schema) {
+                    if ($tmp_schema == $td_entity_name.'.json') {
+                        $schema = json_decode(file_get_contents( $configuration['solution']['schemas']['folder'].'/'.$tmp_schema ),true);
+                    }
+                }
+            }
+        }
+
+
+        $types = [];
+        if (!empty($schema)) {
+            foreach ($schema as $key => $data) {
+                if (isset($data['input']['type'])) {
+                    $types []= [
+                        'type' => '\''.$key.'\' => '.$data['input']['type'].','
+                    ];
+                }
+            }
+        }
+
+
+        $stub = $mustache->render($stub, [
+            'td_entity' => lcfirst($td_entity_name),
+            'Td_entity' => ucfirst($td_entity_name),
+            'function_name' => lcfirst($function_name),
+            'types' => $types,
+        ]);
+        
+        file_put_contents($struct_path, $stub);
         
 
     }
@@ -111,13 +128,22 @@ class StrictType
     }
     
     /**
-     * Get code and save to disk
-     * @return mixed
-     * @throws \Exception
+     * return the names of all events from subscriber folder. (assumes that the namespace conventions are applied)
+     *
+     * @return array
      */
-    public function save()
+    public function getFolderFiles($absolute_folder_path)
     {
-        //
+        $files = scandir($absolute_folder_path);
+
+        $filenames = [];
+        foreach ($files as $file) {
+            if ($file != '.' && $file != '..') {
+                $filenames []= str_replace('.php','',$file);
+            }
+        }
+
+        return $filenames;
     }
 
 }
