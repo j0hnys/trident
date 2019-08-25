8e3c44e80cb4fba64f161d31d09def18ed5462a5
j0hnys <contact_me@johnstamkos.com>
2019-08-25 19:55:44 +0300

    refresh workflow logic function

--- src/Console/Commands/RefreshWorkflowLogicFunction.php
+++ src/Console/Commands/RefreshWorkflowLogicFunction.php
@@ -4,43 +4,38 @@ namespace j0hnys\Trident\Console\Commands;
 
 use Illuminate\Console\Command;
 use j0hnys\Trident\Builders\Refresh;
-use j0hnys\Trident\Base\Constants\Declarations;
 
-
-class RefreshWorkflowRestfulCrud extends Command
+class RefreshWorkflowLogicFunction extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = "trident:refresh:workflow_restful_crud {name} {--functionality_schema_path=} {--resource_schema_path=} {--validation_schema_path=} {--strict_type_schema_path=} ";
+    protected $signature = "trident:refresh:workflow_logic_function {entity_name} {function_name} {--functionality_schema_path=} {--resource_schema_path=} {--validation_schema_path=} {--strict_type_schema_path=}";
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Refresh a workflow with the accompanied restful crud';
-    
-    /**
-     * @var Refresh\WorkflowRestfulCrud
-     */
-    private $crud_workflow_refresh;
+    protected $description = 'Refresh a workflow logic function';
 
-    public function __construct(Refresh\WorkflowRestfulCrud $crud_workflow_refresh = null)
+    /**
+     * @var Refresh\WorkflowLogicFunction
+     */
+    private $workflow_logic_function;
+
+    public function __construct(Refresh\WorkflowLogicFunction $workflow_logic_function = null)
     {
         parent::__construct();
 
-        $Declarations = new Declarations();
-        $Declarations->get();
-
-        $this->crud_workflow_refresh = new Refresh\WorkflowRestfulCrud();
-        if (!empty($crud_workflow_refresh)) {
-            $this->crud_workflow_refresh = $crud_workflow_refresh;
+        $this->workflow_logic_function = new Refresh\WorkflowLogicFunction();
+        if (!empty($workflow_logic_function)) {
+            $this->workflow_logic_function = $workflow_logic_function;
         }
     }
-
+    
     /**
      * Execute the console command.
      *
@@ -49,19 +44,18 @@ class RefreshWorkflowRestfulCrud extends Command
     public function handle()
     {
         try {
-            $options = [];
-            $name = $this->argument('name');
+            $entity_name = $this->argument('entity_name');
+            $function_name = $this->argument('function_name');
             $options['functionality_schema_path'] = $this->option('functionality_schema_path') ? $this->option('functionality_schema_path') : '';
             $options['resource_schema_path'] = $this->option('resource_schema_path') ? $this->option('resource_schema_path') : '';
             $options['validation_schema_path'] = $this->option('validation_schema_path') ? $this->option('validation_schema_path') : '';
             $options['strict_type_schema_path'] = $this->option('strict_type_schema_path') ? $this->option('strict_type_schema_path') : '';
-                        
             
-            //workflow building
-            $crud = $this->crud_workflow_refresh->refresh($name, $options, $this);
 
+            $builders = $this->workflow_logic_function->generate($entity_name, $function_name, $options, $this);
             
-            $this->info($name.' workflow restful crud successfully refreshed');
+
+            $this->info($entity_name.' '.$function_name.' workflow logic function successfully refreshed');
             
         } catch (\Exception $ex) {
             $this->error($ex->getMessage() . ' on line ' . $ex->getLine() . ' in ' . $ex->getFile());
