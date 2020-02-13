69cf6ab9c7effc31fbfdbf15e3952837526b0f41
j0hnys <contact_me@johnstamkos.com>
2020-02-13 22:22:41 +0200

    - refresh WorkflowLogicFunction updated endpoint  - minor cleanup

--- src/Builders/Refresh/WorkflowLogicFunction.php
+++ src/Builders/Refresh/WorkflowLogicFunction.php
@@ -5,6 +5,8 @@ namespace j0hnys\Trident\Builders\Refresh;
 use Illuminate\Console\Command;
 use j0hnys\Trident\Base\Storage\Disk;
 use j0hnys\Trident\Base\Storage\Trident;
+use j0hnys\Trident\Base\Constants\Trident\Functionality;
+use j0hnys\Trident\Base\Constants\Trident\FolderStructure;
 
 class WorkflowLogicFunction
 {
@@ -23,6 +25,8 @@ class WorkflowLogicFunction
         if (!empty($storage_trident)) {
             $this->storage_trident = $storage_trident;
         }
+        $this->functionality_definition = new Functionality();
+        $this->folder_structure = new FolderStructure();
     }
     
     /**
@@ -69,8 +73,80 @@ class WorkflowLogicFunction
             '--schema_path' => $options['resource_schema_path'],
             '--force' => true,
         ]);
+
+        //
+        //update routes
+        $functionality_schema = [];
+        if ($options['functionality_schema_path']) {
+            if (!empty($options['functionality_schema_path'])) {
+                $functionality_schema = json_decode( $this->storage_disk->readFile( $options['functionality_schema_path'] ),true);
+                $this->functionality_definition->check($functionality_schema, 'schema');
+
+                if (isset($functionality_schema['endpoint'])) {
+                    $this->functionality_definition->check($functionality_schema, 'endpoint');
+                }
+            }
+        }
+
+        if (isset($functionality_schema['endpoint'])) {
+            $this->updateRoutes($td_entity_name, $function_name, $functionality_schema);
+        }
     }
 
+
+    public function updateRoutes(string $td_entity_name, string $function_name, array $functionality_schema)
+    {
+        $this->folder_structure->checkPath('routes/trident.php');
+        $trident_resource_routes_path = $this->storage_disk->getBasePath() . '/routes/trident.php';
+        
+        $lines = $this->storage_disk->readFileArray($trident_resource_routes_path); 
+
+        $auth_group_start_line = 0;
+        $auth_group_end_line = 0;
+        $endpoint_exist = false;
+        $endpoint_line = 0;
+
+        //remove existing endpoint if exists
+        foreach ($lines as $i => $line) {
+            if (strpos($line, $functionality_schema['endpoint']['uri']) !== false) {
+                $endpoint_exist = true;
+                $endpoint_line = $i;
+            }
+        }
+        if ($endpoint_exist) {
+            array_splice($lines, $endpoint_line, 1);
+        }
+
+
+        foreach ($lines as $i => $line) {
+            if (strpos($line, "Route::middleware(['auth'])") === 0) {
+                $auth_group_start_line = $i;
+            }
+            if (strpos($line, "});") === 0) {
+                $auth_group_end_line = $i;
+            }
+        }
+
+        $http_method = '';
+        if ($functionality_schema['endpoint']['type'] === 'create') {
+            $http_method = 'post';
+        } else if ($functionality_schema['endpoint']['type'] === 'read') {
+            $http_method = 'get';
+        } else if ($functionality_schema['endpoint']['type'] === 'update') {
+            $http_method = 'put';
+        } else if ($functionality_schema['endpoint']['type'] === 'delete') {
+            $http_method = 'delete';
+        }
+        $line = "Route::".$http_method."('".$functionality_schema['endpoint']['uri']."', 'Trident\\".$td_entity_name."Controller@".$function_name."');";
+        if ($functionality_schema['endpoint']['group'] === 'auth') {
+            array_splice($lines, $auth_group_end_line, 0, ['    '.$line, "\r\n"]);
+        } else {
+            array_splice($lines, count($lines), 0, [$line]);
+        }
+
+
+        $this->storage_disk->writeFileArray($trident_resource_routes_path, $lines); 
+    }
     
     
 
