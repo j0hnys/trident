fb8da3e375a43b7b8b80284e41cdb1f1dde4017c
j0hnys <contact_me@johnstamkos.com>
2020-04-01 22:57:55 +0300

    - generation of ControllerFunction adds "uses" automatically on top of the file

--- src/Builders/Crud/ControllerFunction.php
+++ src/Builders/Crud/ControllerFunction.php
@@ -2,6 +2,9 @@
 
 namespace j0hnys\Trident\Builders\Crud;
 
+use PhpParser\Error;
+use PhpParser\ParserFactory;
+use PhpParser\{Node, NodeFinder};
 use j0hnys\Trident\Base\Storage\Disk;
 use j0hnys\Trident\Base\Constants\Trident\FolderStructure;
 
@@ -50,6 +53,49 @@ class ControllerFunction
         $this->storage_disk->writeFile($controller_path, $stub, [
             'append_file' => true
         ]);
+
+        //
+        //update dependencies in use
+        $this->addDependenciesInUses($controller_path, $td_entity_name, $function_name);
+    }
+
+    public function addDependenciesInUses(string $controller_path, string $td_entity_name, string $function_name): void
+    {
+        $code = $this->storage_disk->readFile($controller_path); 
+        $lines = $this->storage_disk->readFileArray($controller_path); 
+
+        //code analysis
+        $parser = (new ParserFactory)->create(ParserFactory::PREFER_PHP7);
+        try {
+            $ast = $parser->parse($code);
+        } catch (Error $error) {
+            echo "Parse error: {$error->getMessage()}\n";
+            return;
+        }
+
+        $analysis_result = (object)[
+            'used_namespaces' => [],
+        ];
+
+        $nodeFinder = new NodeFinder;
+        $nodeFinder->find($ast, function(Node $node) use (&$analysis_result){
+            if ($node instanceof Node\Stmt\Use_) {
+                $analysis_result->used_namespaces []= $node->uses[0];
+            }
+        });
+
+        //use addition in code
+        $use_validation_string = 'use App\Trident\Workflows\Validations\\'.$td_entity_name.$function_name.'Request;'."\r\n";
+        $use_struct_string = 'use App\Trident\Workflows\Schemas\Logic\\'.$td_entity_name.'\Typed\Struct'.$function_name.$td_entity_name.';'."\r\n";
+
+        $start_line = ($analysis_result->used_namespaces[count($analysis_result->used_namespaces)-1])->getStartLine();
+        $end_line = ($analysis_result->used_namespaces[count($analysis_result->used_namespaces)-1])->getEndLine();
+
+        array_splice($lines, $start_line, 0, $use_validation_string);
+        array_splice($lines, ($start_line+1), 0, $use_struct_string);
+
+        //update file
+        $this->storage_disk->writeFileArray($controller_path, $lines); 
     }
     
 
